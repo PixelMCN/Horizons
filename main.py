733e26cfdaddd20a11e5a1369a2ba8cefabb1747
Koushik Sikdar <131791502+KoushikS6@users.noreply.github.com>
2025-02-21 15:20:58 +0000

    few changes in main.py

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ import os
 load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
-client = commands.Bot(command_prefix='!',intents=discord.Intents.all())
+intents = discord.Intents.default()
+intents.members = True
+
+client = commands.Bot(command_prefix='!',intents=intents)
 
 @client.event
 async def on_ready():
@@ -22,5 +25,9 @@ async def ping(ctx):
 @client.command()
 async def hello(ctx):
     await ctx.send('Hello!')
+
+@client.event
+async def on_member_join(member):
+    print(f'{member} has joined the server.')
 
 client.run(DISCORD_TOKEN)
