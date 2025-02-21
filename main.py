e09cafe80495b6a1acb4214f2ade8232b5d41c99
Koushik Sikdar <131791502+KoushikS6@users.noreply.github.com>
2025-02-21 15:06:04 +0000

    few changes

--- main.py
+++ main.py
@@ -6,3 +6,21 @@ import os
 
 load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+
+client = commands.Bot(command_prefix='!',intents=discord.Intents.all())
+
+@client.event
+async def on_ready():
+    print('Bot is ready.')
+    print(f'Logged in as {client.user}')
+    print('---------------------------------')
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+
+@client.command()
+async def hello(ctx):
+    await ctx.send('Hello!')
+
+client.run(DISCORD_TOKEN)
